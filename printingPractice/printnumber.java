c2b1ba1566cbe8705a3f9e8162a68eaf26b478e1
ZBarronn <149598181+ZBarronn@users.noreply.github.com>
2023-11-13 14:55:52 +0000

    code

---
+++ printingPractice/printnumber.java
@@ -0,0 +1,15 @@
+class printnumber
+{
+    public static void main (String args[])
+    {
+        System.out.println(" Hello! ");
+        System.out.println(" ");
+        System.out.println("1");
+        System.out.println("1 2");
+        System.out.println("1 2 3");
+        System.out.println("1 2 3 4");
+        System.out.println("1 2 3 4 5");
+        System.out.println(" ");
+        System.out.println("Thank you bye!");
+    }
+}
