915a5f7782426d87a5698547d0ab2c8f3a49393f
ZBarronn <149598181+ZBarronn@users.noreply.github.com>
2023-11-13 14:58:34 +0000

    code

--- printingPractice/printstory.java
+++ printingPractice/printstory.java
@@ -3,11 +3,15 @@ class printstory
     public static void main (String args[])
     {
         System.out.println("A story about a dino ");
+        System.out.println(" ");
         System.out.println("Once upon a time... ");
+        System.out.println(" ");
         System.out.println("There was a dino named Jamie");
         System.out.println("Jamie was a pink and green dino");
         System.out.println("One day Jamie found an orange");
         System.out.println("Jamie loved the orange so much she named it Jerry");
         System.out.println("Both Jamie and Jerry lived happily ever after.");
+        System.out.println(" ");
+        System.out.println("Thank you for reading!");
     }
 }
