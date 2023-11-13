d8405dfa6ce08e94a4617c9d44499ee8c3c4ed31
ZBarronn <149598181+ZBarronn@users.noreply.github.com>
2023-11-13 14:33:54 +0000

    code

---
+++ printingPractice/printpractice.java
@@ -0,0 +1,13 @@
+class printpractice
+{
+    public static void main (String args[])
+    {
+        System.out.println(" Hello! ");
+        System.out.println(" ");
+        System.out.println(" User's name: Zoen");
+        System.out.println(" ");
+        System.out.println(" User's age: 14");
+        System.out.println(" ");
+        System.out.println(" Bye! ");
+    }
+}
