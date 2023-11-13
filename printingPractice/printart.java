927102f270a951e4a6138c63c64c4c109ff4ceca
ZBarronn <149598181+ZBarronn@users.noreply.github.com>
2023-11-13 15:21:18 +0000

    codes

---
+++ printingPractice/printart.java
@@ -0,0 +1,16 @@
+class printart
+{
+    public static void main (String args[])
+    {
+        System.out.println("     _____");
+        System.out.println("   / 7     >");
+        System.out.println("  |  _    _|");
+        System.out.println("  /-(== * ``)" );
+        System.out.println(" /         | /````)");
+        System.out.println(" |  `      )/ /```");
+        System.out.println("/   |  |  |  /");
+        System.out.println("|   |  |  | /");
+        System.out.println("/   |  )  )/");
+        System.out.println("````````````");
+    }
+}
