f0fea9dc20f51e7f3d2952b9bb579388352d8e8f
ZBarronn <149598181+ZBarronn@users.noreply.github.com>
2023-11-15 14:18:44 +0000

    code

---
+++ printingPractice/printpatternone.java
@@ -0,0 +1,9 @@
+class printpatternone
+{
+    public static void main (String args[])
+    {
+        System.out.println("*  *  * ");
+        System.out.println(" *   * ");
+        System.out.println("   * ");
+    }
+}
---
+++ printingPractice/printpatterntwo.java
@@ -0,0 +1,11 @@
+class printpatterntwo
+{
+    public static void main (String args[])
+    {
+        System.out.println("*      * ");
+        System.out.println(" *   * ");
+        System.out.println("   * ");
+        System.out.println(" *   * ");
+        System.out.println("*      * ");
+    }
+}
