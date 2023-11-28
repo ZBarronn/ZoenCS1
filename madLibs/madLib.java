bf433f89245d0b45f8e74095b9226b81896bd6cc
ZBarronn <149598181+ZBarronn@users.noreply.github.com>
2023-11-28 23:32:07 +0000

    E

--- madLibs/madLib.java
+++ madLibs/madLib.java
@@ -18,7 +18,7 @@ class madLib
         String partOfBody2 = "partofbody2";
         String plNoun6 = "plnoun6";
         String adverb = "adverb";
-        String plNoun7 = "plnoun.";
+        String plNoun7 = "plnoun";
         System.out.print("Throughout last night's " + noun + ", the cheering for the performance of Pearl " + food + " was so deafening, you had to hold your " + plNoun + " over your " + plNoun2 + " . Many well-known " + plNoun3 + " are calling it the " + adjective + " concert of the decade. For their opening number, the band played their hit song,“,” followed by their " + adjective2 + " song called of “I Really Love My " + noun2 + ".” Then, as a tribute to the Beatles, they played several " + plNoun4 + " from the hit album, Sergeant Pepper's Lonely " + partOfBody + ". Unfortunately, throughout the performance, lead singer " + person + " moved about the stage like a caged " + animal + ", singing at the top of his/her " + plNoun5 + ", giving this critic a terrible " + partOfBody2 + " ache. However, the concert ended with the audience standing on their " + plNoun6 + " and applauding " + adverb + ", forcing the group to come back for three more " + plNoun7 + ".");
     }
 }
