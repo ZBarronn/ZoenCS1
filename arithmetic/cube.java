7ea7fb25108c934e3b79d2fe392c71e4cff26b7d
ZBarronn <149598181+ZBarronn@users.noreply.github.com>
2023-12-11 15:21:32 +0000

    w

---
+++ arithmetic/cube.java
@@ -0,0 +1,8 @@
+class cube
+{
+    public static void main (String args[])
+    {
+        int num1 = 2;
+        System.out.print(num1 * num1 * num1);
+    }
+}
---
+++ arithmetic/square.java
@@ -0,0 +1,8 @@
+class square
+{
+    public static void main (String args[])
+    {
+        int num1 = 3;
+        System.out.print(num1 * num1);
+    }
+}
