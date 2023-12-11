27c0c01592c19b2e62bdad483f3aca0adb3d1f3c
ZBarronn <149598181+ZBarronn@users.noreply.github.com>
2023-12-11 15:48:36 +0000

    yes

---
+++ arithmetic/fahToCel.java
@@ -0,0 +1,10 @@
+class fahToCel
+{
+    public static void main (String args[])
+    {
+        double fahrenheit = 1;
+        double fminus32 = fahrenheit - 32;
+        double celsius = fminus32 * 5/9;
+        System.out.print(celsius);
+    }
+}
