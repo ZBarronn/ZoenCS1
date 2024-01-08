82b58178b9699822e2e3393b59bd941f545b9416
ZBarronn <149598181+ZBarronn@users.noreply.github.com>
2024-01-08 16:50:53 +0000

    yes

--- arithmetic/volume.java
+++ arithmetic/volume.java
@@ -5,10 +5,12 @@ class volume
         double radius = 5;
         double height = 8;
         double square = (radius * radius);
-        double pi = (square * 3.14159);
-        double num = (pi * height);
+        double pi = (3.14159);
+        double num = ((pi* square) * height);
+        double num2 = ((2 * pi) * radius);
+        double surfaceArea = (num2 * (radius + height));
 
-        System.out.print(num);
+        System.out.print("the volume is " + num + " and the surface area is " + surfaceArea + ".");
 
     }
 }
