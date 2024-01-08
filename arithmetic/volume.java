204c1d66362c8a59edaeea61ccb496dd42134e40
ZBarronn <149598181+ZBarronn@users.noreply.github.com>
2024-01-08 16:40:18 +0000

    yes

---
+++ arithmetic/volume.java
@@ -0,0 +1,14 @@
+class volume
+{
+    public static void main (String args[])
+    {
+        double radius = 5;
+        double height = 8;
+        double square = (radius * radius);
+        double pi = (square * 3.14159);
+        double num = (pi * height);
+
+        System.out.print(num);
+
+    }
+}
---
+++ arithmetic/wholeNumber.java
@@ -0,0 +1,9 @@
+class wholeNumber
+{
+    public static void main (String args[])
+    {
+       double quotient = (85/9);
+       double remainder = (85%9);
+       System.out.print ("quotient is " + quotient + " and the remainder is " + remainder + ".");
+    }
+}
