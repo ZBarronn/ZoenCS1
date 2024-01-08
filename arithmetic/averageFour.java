01ce90786ce24772752f7790cdaeb9831dc34624
ZBarronn <149598181+ZBarronn@users.noreply.github.com>
2024-01-08 16:20:00 +0000

    yes

---
+++ arithmetic/averageFour.java
@@ -0,0 +1,12 @@
+class averageFour
+{
+    public static void main (String args[])
+    {
+        double num1 = 15;
+        double num2 = 5;
+        double num3 = 10;
+        double num4 = 3;
+        double addedNum = num1 + num2 + num3+ num4;
+        System.out.print( addedNum / 4);
+    }
+}
---
+++ arithmetic/squareAndHalf.java
@@ -0,0 +1,11 @@
+class squareAndHalf
+{
+    public static void main (String args[])
+    {
+        double num1 = 3;
+        double square = (num1 * num1);
+        double num = (square/2);
+        System.out.print(num);
+
+    }
+}
