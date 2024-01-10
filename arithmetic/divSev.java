431ee4f4b8422a96125be3bbeab104a056e5f5c8
ZBarronn <149598181+ZBarronn@users.noreply.github.com>
2024-01-10 16:22:54 +0000

    yes

---
+++ arithmetic/divSev.java
@@ -0,0 +1,18 @@
+class divSev
+{
+    public static void main (String args[])
+    {
+       double num = (15);
+       double seven = (7);
+       double div = (num%seven);
+       
+       if(div == 0.0)
+       {
+        System.out.print(num + " is evenly divisible by 7.");
+    }
+    else
+    {
+        System.out.print(num + " is not evenly divisible by 7.");
+    }
+    }
+}
