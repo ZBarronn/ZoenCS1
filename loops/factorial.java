175062000d4664100f64c0e4f5d8995ee3490852
ZBarronn <149598181+ZBarronn@users.noreply.github.com>
2024-02-21 15:59:14 +0000

    yes

---
+++ loops/factorial.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class factorial {
+    public static void main(String args[]) {
+        Scanner numScanner = new Scanner(System.in);
+
+        System.out.print("Please enter a number: ");
+        int num = numScanner.nextInt();
+        int num2 = 1;
+        int num3 = 1;
+
+        while (num2 <= num) {
+            num3 *= num2;
+            num2++;
+        }
+
+        System.out.println("The factorial of " + num + " is " + num3);
+    }
+}
